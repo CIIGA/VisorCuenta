3eeb093be327c39700452a77e6c24ffb1d2482ad
dieg00s0ri0 <diego9.5.2000jios@gmail.com>
2023-07-24 09:36:54 -0600

    0936240723

--- php/gestiones/crud/validaciones.php
+++ php/gestiones/crud/validaciones.php
@@ -1,26 +1,27 @@
 <?php
-require "../include/awsv2/vendor/autoload.php";
+// require "../include/awsv2/vendor/autoload.php";
 
-use Aws\S3\S3Client;
-use Aws\S3\Exception\S3Exception;
-// echo date('TH:i:s');
+// $key=$_GET['key'];
+
+// use Aws\S3\S3Client;
+// use Aws\S3\Exception\S3Exception;
+// // echo date('TH:i:s');
 
 
-require "funcionesS3.php";
+// require "funcionesS3.php";
 
-$s3 = S3Client::factory([
-    'version' => '2006-03-01',
-    'region' => 'us-east-1',
-    'credentials' => [
-        'key' => 'AKIAQAVQA6VN3G4QA5GC',
-        'secret' => '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',
-    ]
-]);
-$bucket='fotos-implementta-movil';
-$key='20407142023-07-12 01:07:55.000Z';
+// $s3 = S3Client::factory([
+//     'version' => '2006-03-01',
+//     'region' => 'us-east-1',
+//     'credentials' => [
+//         'key' => 'AKIAQAVQA6VN3G4QA5GC',
+//         'secret' => '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',
+//     ]
+// ]);
+// $bucket='fotos-implementta-movil';
 
-$existe=exists($s3,$bucket,$key);
-// $url=url($s3,$key);
-// $delete=delete($s3,$bucket,$key);
-echo $existe;
+// $existe=exists($s3,$bucket,$key);
+// // $url=url($s3,$key);
+// // $delete=delete($s3,$bucket,$key);
+// echo $existe;
 ?>
