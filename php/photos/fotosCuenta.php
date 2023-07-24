c1c79ce6437b321770daeec1c1c7cc3cc0ec9929
CIIGA <ciigasistemas@gmail.com>
2023-07-24 13:54:05 -0600

    iconos y color

--- php/photos/fotosCuenta.php
+++ php/photos/fotosCuenta.php
@@ -1,8 +1,8 @@
 <div class="container container_foto" id="fotoCuenta">
-    <h3 class="text-center">
-        <img width="30" height="30" src="https://img.icons8.com/external-others-inmotus-design/30/external-Photo-Gallery-vkontakte-others-inmotus-design.png" alt="external-Photo-Gallery-vkontakte-others-inmotus-design" />
+    <h4 class="text-center p-2">
+    <i class="fa-solid fa-camera"></i>
         Fotos
-    </h3>
+    </h4>
     <div class="row" id="photoRow"></div>
 </div>
 <script src="js/photosData.js"></script>
--- php/tasks/tareasCuenta.php
+++ php/tasks/tareasCuenta.php
@@ -1,15 +1,15 @@
-<div class="container" id="TaskCuenta">
+<div class="container container_tasks" id="TaskCuenta">
     <div class="row gap-2">
         <div class="col-md-5 container_foto">
-            <img width="30" height="30" src="https://img.icons8.com/cute-clipart/30/task.png" alt="task" />
+            <i class="fa-solid fa-bars-progress"></i>
             Tareas Pendientes
             <div class=" row" id="tareasContainer">
                 <!-- Tarjetas de tareas se generarán aquí -->
             </div>
-            
+
         </div>
         <div class="col-md-5 container_foto">
-            <img width="30" height="30" src="https://img.icons8.com/fluency/30/tasklist.png" alt="tasklist" />
+            <i class="fa-solid fa-list-check"></i>
             Tareas Realizadas
             <div class="row" id="tareasContainerRealizadas">
                 <!-- Tarjetas de tareas se generarán aquí -->
