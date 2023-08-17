da18de9fc0e410aa033fa26567446001763e868d
Adrian <36174275+jennypc@users.noreply.github.com>
2023-08-17 12:57:30 -0600

    115817082023

--- index.php
+++ index.php
@@ -37,7 +37,9 @@ if(isset($_SESSION['userASP']) and isset($_SESSION['plazaBD']) and isset($_SESSI
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
   <link href="css/index.css" rel="stylesheet" />
   <link href="img/fontawesomeV6/css/all.css" rel="stylesheet" />
-  <title>Index</title>
+  <title>Busqueda</title>
+  <link rel="icon" href="img/implementtaIcon.png">
+
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
